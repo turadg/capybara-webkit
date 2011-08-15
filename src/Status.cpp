f7eaf3cbfd368a23a527f9794ab65b5794684733
Matthew Mongeau <halogenandtoast@gmail.com>
2011-08-15 10:14:41 -0400

    Int to string using QString::number

--- src/Status.cpp
+++ src/Status.cpp
@@ -7,8 +7,7 @@ Status::Status(WebPage *page, QObject *parent) : Command(page, parent) {
 
 void Status::start(QStringList &arguments) {
   Q_UNUSED(arguments);
-  std::stringstream sstr(std::stringstream::in | std::stringstream::out);
-  sstr << page()->getLastStatus();
-  emit finished(new Response(true, QString(sstr.str().c_str())));
+  int status = page()->getLastStatus();
+  emit finished(new Response(true, QString::number(status)));
 }
 
